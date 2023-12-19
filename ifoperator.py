6a1e95d628a1a820000bebdf8691784a5ba021bc
Karsaninvu <karsanin.v.u.87@mail.ru>
2023-12-19 22:10:05 +0400

    base

---
+++ ifoperator.py
@@ -0,0 +1,35 @@
+x = 55
+
+print('Hello')
+if x < 0 :
+    print('Меньше нуля')
+print('Досвидания')
+
+a, b = 10, 5
+
+if (a > b):
+    print('a>b')
+if a>b and a > 0:
+    print('успех')
+if (a>b) and (a > 0 or b < 1000):
+    print('Успех')
+if 5 < b and b < 10:
+    print('Успех')
+
+if '34' > '123':
+    print('Успех')
+
+if '123' > '12':
+    print('успех')
+
+if [1, 2] > [1, 1]:
+    print('успех')
+
+if '6' > 5:
+    print('Успех')
+
+if [5, 6] > 5:
+    print('успех')
+
+if '6' != 5:
+    print('успех')
