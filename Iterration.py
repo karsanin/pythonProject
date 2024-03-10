baee14631cd6342ca5612928d1cb6c5a2b37e508
Karsaninvu <karsanin.v.u.87@mail.ru>
2024-03-10 13:11:25 +0400

    итераторы

--- Iterration.py
+++ Iterration.py
@@ -3,23 +3,25 @@ class EvenNumbers:
     def __init__(self, start, end):
         self.start = start
         self.end = end
-        self.i = 0
+        self.n = (self.end - self.start)/2
 
     def __iter__(self):
         self.i = 0
+        self.n += 1
+        self.start -= 2
         return self
 
     def __next__(self):
         self.i += 1
-        if self.i <= self.end - self.start:
-            if self.start % 2 == 0:
-                self.start += 1
-                return self.start
+        self.start += 1
+        if self.start % 2 == 0:
+            if self.i > self.n:
+                raise StopIteration()
+            self.start += 1
+        return self.start + 1
 
-        raise StopIteration()
 
-
-en = EvenNumbers(10, 25)
+en = EvenNumbers(0, 1)
 for a in en:
     print(a)
 
