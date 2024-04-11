00c71a30cf75d24187af7bb254c8aa0a058d31b3
Karsaninvu <karsanin.v.u.87@mail.ru>
2024-04-11 20:58:54 +0400

    Голова кипит, не могу сделать запись о том что клиент попал в очередь

--- cafe.py
+++ cafe.py
@@ -12,57 +12,52 @@ class Table:
 class Customer(threading.Thread):
     def __init__(self, name):
         super().__init__()
-        # self.cafe = cafe
+        self.cafe = cafe
         self.name = name
 
     def run(self):
-        # print(f"Посетитель обслуживается")
-        time.sleep(5)
+        print(f"Посетитель {self.name} прибыл")
+        self.cafe.serve_customer(self)
 
 
 class Cafe:
     def __init__(self, tables):
         self.tables = tables
-        self.queue = Queue(maxsize=2)
-        self.costumers = []
+        self.queue = Queue()
+        # self.costumers = []
         self.count = 0
 
-    def customer_arrival(self, customer_qnt):
-        for i in range(customer_qnt):
-            obj = Customer(f'{i}')
-            self.costumers.append(obj)
+    def customer_arrival(self):
+        for i in range(5):
+            obj = Customer(f'{i+1}')
+            obj.start()
             time.sleep(1)
-        return self.costumers
+
+
 
     def serve_customer(self, customer):
-        for table in tables:
-            if not table.is_busy:
-                for cust in customer:
-                    self.count += 1
-                    print(f'Посетитель №{self.count} обслуживается')
+        self.queue.put(customer.name)
+        while not self.queue.empty():
+            for table in tables:
+                if not table.is_busy:
                     table.is_busy += 1
-                    cust.start()
-                    print(f'Посетитель №{self.count} закончил')
-                    cust.join()
+                    name = self.queue.get()
+                    print(f'Посетитель #{name} сел за стол № {table.number}, обслуживается')
+                    time.sleep(5)
+                    print(f'Посетитель #{name} закончил, стол № {table.number} свободен')
                     table.is_busy -= 1
-            else:
-                print("yttttttn")
-        else:
-            print(f'may go to the line')
-
 
 
 table1 = Table(1)
 table2 = Table(2)
-tables = [table1, table2]
+table3 = Table(3)
+tables = [table1, table2, table3]
 
 cafe = Cafe(tables=tables)
 
-atr = cafe.customer_arrival(3)
-cafe.serve_customer(atr)
-# customer_serve_customer = threading.Thread(target=cafe.serve_customer(atr))
-# customer_serve_customer.start()
-#
-# # Ожидаем завершения работы прибытия посетителей
-# customer_serve_customer.join()
 
+customer_arrival_thread = threading.Thread(target=cafe.customer_arrival)
+customer_arrival_thread.start()
+
+# Ожидаем завершения работы прибытия посетителей
+customer_arrival_thread.join()
